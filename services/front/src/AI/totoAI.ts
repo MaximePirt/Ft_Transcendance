d0e744b6c20514632392b86ef864fb98cbc2db74
Thibault <thibault@macbookair.home>
2025-11-28 22:32:51 +0100

    train toto

--- services/front/src/AI/totoAI.ts
+++ services/front/src/AI/totoAI.ts
@@ -34,8 +34,8 @@ let paddle2XY = paddle2.getBoundingClientRect();
 const fieldXY = field.getBoundingClientRect();
 let scoreP1 = document.getElementById('score1') as HTMLElement;
 let scoreP2 = document.getElementById('score2') as HTMLElement;
-let dx = 5;
-let dy = 5;
+let dx = 1;
+let dy = 1;
 const border = 10;
 
 // 400 possibilities because of the field height
@@ -64,7 +64,7 @@ let n_training = 100000;
 let n_episodes = 100;
 let max_epsilon = 1.0;
 let min_epsilon = 0.05;
-let decay_rate = 0.001;
+let decay_rate = 0.01;
 let qTable: number[][];
 let reward = 0;
 
@@ -104,41 +104,39 @@ function epsilonGreedyPolicy(epsilon: number, state: number) {
 	let floor_s = Math.floor(state);
 	let result = 0;
 
+	console.log(`epsilon: ${epsilon}, state: ${state}`);
 	if (random_nb > epsilon) {
+		console.log("random > epsilon");
 		result = Math.max(...qTable[floor_s]);
 	} else {
+		console.log("random result");
 		let randomI = Math.floor(Math.random() * 3);
 		let randomJ = Math.floor(Math.random() * max_s);
 
 		result = qTable[randomJ][randomI];
 	}
-
 	return result;
 }
 
+let action: number;
+let state: number;
+let new_state: number;
+let i: number;
 function totoAI() {
-	let action;
-
-	for (let i: number = 0; i < n_episodes; i++) {
+	for (i = 0; i < n_episodes; i++) {
 		let epsilon = min_epsilon + (max_epsilon - min_epsilon) * Math.exp(-decay_rate * i);
-		let state = Math.floor(paddle2XY.y);
-		let game = moveBall();
-		while (game != 0 && game != 1) {
-			let new_state = Math.floor(paddle2XY.y);
-
-			action = epsilonGreedyPolicy(epsilon, state);
-			movePaddle(action);
-			//Bellman equation
-
-			qTable[state][action] = qTable[state][action] + alpha * (reward + gamma * Math.max(...qTable[new_state]) - qTable[state][action]);
-			state = new_state;
-		}
+		state = Math.floor(paddle2XY.y);
+		action = epsilonGreedyPolicy(epsilon, state);
+		moveBall();
+		movePaddle(action);
+		new_state = Math.floor(paddle2XY.y);
+		state = new_state;
 	}
 }
 
 function movePaddle(action: number) {
 	if (action === 0) {
-		paddle1XY.y -= 15;
+		paddle2XY.y -= 15;
 		if (paddle2XY.y <= 0)
 			paddle2XY.y = 0;
 	}
@@ -150,7 +148,7 @@ function movePaddle(action: number) {
 	if (action === 2)
 		paddle2XY.y += 0;
 	paddle2.style.top = `${paddle2XY.y}px`;
-	requestAnimationFrame(movePaddle);
+	// requestAnimationFrame(movePaddle);
 }
 
 function relativeAngle(paddle: DOMRect) {
@@ -172,7 +170,7 @@ function moveBall() {
 	ballXY.x += dx;
 	ballXY.y -= dy;
 	paddle1.remove();
-	
+
 	if (ballXY.x + ballXY.height >= fieldXY.width) {
 		markPlayer1 = true;
 		dx *= -1;
@@ -186,6 +184,7 @@ function moveBall() {
 		markPlayer2 = true;
 		dx *= -1;
 	} else if (collidePaddle2(ballXY, paddle2XY)) {
+		reward = 1;
 		dx *= -1;
 		dx *= Math.cos(relativeAngle(paddle2XY)) + 0.1;
 		dy *= Math.sin(relativeAngle(paddle2XY)) + 0.1;
@@ -193,25 +192,25 @@ function moveBall() {
 	}
 	if (markPlayer2) {
 		score2++;
-		scoreP2.textContent = score2.toString();
-		ballXY.x = 500;
-		ballXY.y = 250;
+		reward = 1;
+		//put Bellman in a function
+		qTable[state][action] = qTable[state][action] + alpha * (reward + gamma * Math.max(...qTable[new_state]) - qTable[state][action]);
+		console.log(`Bellman: ${qTable[state][action]}, state: ${state}, action: ${action}`);
 		markPlayer2 = false;
-		return 1;
-	}
-	if (markPlayer1) {
+		console.log(`i: ${i}`);
+	} else if (markPlayer1) {
 		score1++;
-		scoreP1.textContent = score1.toString();
+		//scoreP1.textContent = score1.toString();
 		ballXY.x = 500;
 		ballXY.y = 250;
+		reward = 0;
+		qTable[state][action] = qTable[state][action] + alpha * (reward + gamma * Math.max(...qTable[new_state]) - qTable[state][action]);
 		markPlayer1 = false;
-		return 0;
 	}
-	requestAnimationFrame(moveBall);
+	// requestAnimationFrame(moveBall);
 	ball.style.left = `${ballXY.x}px`;
 	ball.style.top = `${ballXY.y}px`;
 }
 
-moveBall();
 //heuristicPlayer();
 totoAI();
